8e0ac5da53a016ec41f6c3ddd87307824d14014b
Andrin <andrin.bertschi@gmail.com>
2016-05-11 21:03:48 +0200

    add transformers

---
+++ impl/src/main/java/ch/abertschi/sct/newp/transformer/Transformers.java
@@ -0,0 +1,38 @@
+package ch.abertschi.sct.newp.transformer;
+
+import ch.abertschi.sct.newp.Node;
+
+import java.util.List;
+
+/**
+ * Created by abertschi on 11/05/16.
+ */
+public class Transformers
+{
+    public static void transform(Node node, List<Transformer> transformers, CallContext context)
+    {
+        if (node.isContainer())
+        {
+            node.getChildren().forEach(child -> transform(child, transformers, context));
+        }
+        else
+        {
+            transformers
+                    .stream()
+                    .filter(t -> t.canTransform(context, node.getValue()))
+                    .forEach(t -> node.setValue(t.transform(context, node.getValue())));
+        }
+    }
+
+    public static String transform(String input, List<Transformer> transformers, CallContext context)
+    {
+        for (Transformer transformer : transformers)
+        {
+            if (transformer.canTransform(context, input))
+            {
+                input = transformer.transform(context, input);
+            }
+        }
+        return input;
+    }
+}
